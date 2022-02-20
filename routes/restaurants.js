c96e79c9a7e734f197fe73924a669245c6e8f2bc
Phil Bailey <philbailey@msn.com>
2022-02-20 12:29:40 -0500

    add restaurant routes

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const Restaurant = require("../models/restaurant");
 
 //Route endpoints for restaurants
-//
+
+//-------------------------------
 //Get all
 router.get("/", async (req, res) => {
 	try {
@@ -14,20 +15,70 @@ router.get("/", async (req, res) => {
 	}
 });
 
+//-------------------------------
 //Get one
-router.get("/:id", (req, res) => {
-	res.send(req.params.id);
+router.get("/:id", getRestaurant, (req, res) => {
+	res.json(res.restaurant);
 });
+
+//-------------------------------
 //Create one
-router.post("/", (req, res) => {});
-//Update one
-router.patch("/:id", (req, res) => {});
-//Delete one
-router.delete("/:id", (req, res) => {});
-
-async function get(req, res, next) {
+router.post("/", async (req, res) => {
+	const restuarant = new Restaurant({
+		borough: req.body.borough,
+		cuisine: req.body.cuisine,
+	});
 	try {
-	} catch (err) {}
-}
+		const newRestaurant = await restuarant.save();
+		res.status(201).json(newRestaurant);
+	} catch (err) {
+		res.status(400).json({ message: err.message });
+	}
+});
 
+//-------------------------------
+//Update one
+router.patch("/:id", getRestaurant, async (req, res) => {
+	if (req.body.borough != null) {
+		res.restaurant.borough = req.body.borough;
+	}
+	if (req.body.cuisine != null) {
+		res.restaurant.cuisine = req.body.cuisine;
+	}
+	try {
+		const updated = await res.restaurant.save();
+		res.json(updated);
+	} catch (err) {
+		res.status(400).json({ message: err.message });
+	}
+});
+
+//-------------------------------
+//Delete one
+router.delete("/:id", getRestaurant, async (req, res) => {
+	try {
+		await res.restaurant.remove();
+		res.json({ message: "Deleted" });
+	} catch (err) {
+		res.status(500).json({ message: err.message });
+	}
+});
+
+//=======================Middleware===========================
+async function getRestaurant(req, res, next) {
+	//middleware to add found restaurant to response object
+	let restaurant;
+	try {
+		restaurant = await Restaurant.findById(req.params.id);
+		if (restaurant == null) {
+			return res.status(404).json({ message: "Not found" });
+		}
+	} catch (err) {
+		return res.status(500).json({ message: err.message });
+	}
+	//add to response object
+	res.restaurant = restaurant;
+	next();
+}
+//=============================================================
 module.exports = router;
